2a016a66364ace0f3b79f64380190082bf414391
Carlos Eduardo Falandes <eduardofalandess@gmail.com>
2022-04-10 14:22:28 -0300

    funcao para enviar as informacoes ao mysql

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for, redirect
+from flask_mysqldb import MySQL 
 
 app = Flask('__name__')
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'usuarios_solicitacoes'
+
+mysql = MySQL(app)
 
 
 @app.route('/')
@@ -9,18 +17,34 @@ app = Flask('__name__')
 def tela_de_inicio():
     return render_template('tela de inicio.html')
 
+@app.route('/cadastro.html')
+def pag_cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/cadastro.html', methods= ['POST'])
+def cadastro():
+    email= request.form['email']
+    senha= request.form['senha']
+    confsenha = request.form['confirmacao_senha']
+    ciencia = request.form['ciencia']
+    if senha == confsenha:
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT into usuarios (email_usuario,senha_usuario) VALUES (%s, %s)',(email, senha))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/login.html')
+
 @app.route('/login.html')
 def login():
     return render_template('login.html')
 
-@app.route('/cadastro.html')
-def cadastro():
-    return render_template('cadastro.html')
+@app.route("/tela usuario.html")
+def tela_usuario():
+    return render_template('tela usuario.html')
 
 @app.route("/tela executor.html")
 def tela_executor():
-    nome_da_variavel = "sou foda"
-    return render_template('tela executor.html', variavel=nome_da_variavel)
+    return render_template('tela executor.html')
 
 
 if __name__ == "__main__":

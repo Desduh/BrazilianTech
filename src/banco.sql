afa0f07fd1418567403663b190884f1c990a719b
Carlos Eduardo Falandes <eduardofalandess@gmail.com>
2022-04-28 21:48:50 -0300

    adicao da coluna aceito e delecao dos codigos inuteis

--- src/banco.sql
+++ src/banco.sql
@@ -1,20 +1,16 @@
 create database usuarios_solicitacoes;
 
 use usuarios_solicitacoes;
+
 create table usuarios
 (	codigo_usuario int primary key AUTO_INCREMENT,
 	email_usuario varchar(40) NOT NULL UNIQUE KEY,
     senha_usuario varchar(30) NOT NULL);
 
-select * from usuarios;
 
 create table chamado
 (	codigo_solicitacao int primary key AUTO_INCREMENT,
 	solicitacao varchar(1000),
 	email_usuario varchar(40),
-    data_inicio datetime );
-
-select * from chamado;
-select codigo_solicitacao,solicitacao,email_usuario,data_inicio FROM chamado ORDER BY data_inicio DESC;
-
-
+    data_inicio datetime,
+	aceito varchar(1000));

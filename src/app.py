3efa00cc955cd511c79e1e5e0ce82da54a042674
Desduh <eduardofalandess@gmail.com>
2022-05-04 15:58:08 -0300

    adicao das respostas na funcao do executor

--- src/app.py
+++ src/app.py
@@ -47,7 +47,7 @@ def telausuario():
 @app.route('/telaexecutor')
 def telaexecutor():
     cur = mysql.connection.cursor()
-    users = cur.execute("select codigo_solicitacao,solicitacao,email_usuario,data_inicio,_status FROM chamado ORDER BY data_inicio DESC;")
+    users = cur.execute("select codigo_solicitacao,solicitacao,email_usuario,data_inicio,_status,resposta FROM chamado ORDER BY data_inicio DESC;")
     Details = cur.fetchall()
     return render_template("telaexecutor.html", Details=Details)
 

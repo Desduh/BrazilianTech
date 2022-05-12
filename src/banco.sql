e3061eeaa765583814047a37252f90d0c8c87d75
Desduh <eduardofalandess@gmail.com>
2022-05-12 18:44:15 -0300

    novo banco com correcoes

--- src/banco.sql
+++ src/banco.sql
@@ -7,24 +7,20 @@ create table usuarios
 (	codigo_usuario int primary key AUTO_INCREMENT,
 	email_usuario varchar(40) NOT NULL UNIQUE KEY,
     senha_usuario varchar(30) NOT NULL,
-    funcao int);
+    funcao int,
+    contador_solicitacao int);
     
 insert into usuarios(email_usuario,senha_usuario,funcao) values("administrador@adm","fatec",3);
-
-
-create table distribuicao
-(   executor int primary key,
-    foreign key(executor) REFERENCES usuarios (codigo_usuario),
-    contador int);
     
 
 create table solicitacao
 (	codigo_solicitacao int primary key AUTO_INCREMENT,
 	descricao varchar(1000),
 	email_usuario varchar(40),
+    foreign key(email_usuario) REFERENCES usuarios (email_usuario),
     data_abertura datetime,
-    executor int,
-    foreign key(executor) REFERENCES distribuicao (executor),
+    codigo_usuario int,
+    foreign key(codigo_usuario) REFERENCES usuarios (codigo_usuario),
     _status varchar(30),
     constraint ck_status check(_status in('Aceito','Negado','Aberto')),
 	resposta varchar(1000),
